bc2eababaea185ff33852a0b446c9cd08e22bddc
yxhwxn <officeyh9819@gmail.com>
2024-07-02 02:16:31 +0900

    Chore: migrating existing repository code

--- src/main/java/thegoods/server/member/converter/MemberConverter.java
+++ src/main/java/thegoods/server/member/converter/MemberConverter.java
@@ -1,8 +1,8 @@
-package thegoods.server.member.business;
+package thegoods.server.member.converter;
 
 import thegoods.server.member.domain.Member;
 
-public class MemberMapper {
+public class MemberConverter {
 
     // 기존 레포지토리의 PostConverter에 있는 Follow는 Member 도메인으로 이동 시켰습니다.(추후에 생각해보시고 수정하셔도 됩니다!)
     public static Follow toFollow(Member follower, Member following) {
